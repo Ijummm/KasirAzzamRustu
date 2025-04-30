5b4f662372de7d76af3add5d63ec4ae94adc5480
Azzam - RPL <mazzamoke190@gmail.com>
2025-04-30 11:53:40 +0700

    Add files via upload

---
+++ Transaksi.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaksi extends Model
+{
+    protected $fillable = [
+        'id_barang',
+        'harga_asli',
+        'nominal_diskon',
+        'harga_diskon',
+        'total_pembelian',
+        'keuntungan',
+        'tanggal_pembelian',
+    ];
+
+    protected function casts():array
+    {
+        return [
+            'tanggal_pembelian' => 'date'
+        ];
+    }
+}
